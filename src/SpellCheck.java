53a87a7d021aba62382011296baee65d15e9a9eb
williambeesley <wpbeesley@gmail.com>
2025-09-24 11:46:52 -0700

    v1 - using sets. All test cases passed

--- src/SpellCheck.java
+++ src/SpellCheck.java
@@ -1,24 +1,32 @@
+import java.util.*;
 /**
  * Spell Check
  * A puzzle written by Zach Blick
  * for Adventures in Algorithms
  * At Menlo School in Atherton, CA
  *
- * Completed by: [YOUR NAME HERE]
+ * Completed by: William Beesley
  * */
 
 public class SpellCheck {
-
-
-    /**
-     * checkWords finds all words in text that are not present in dictionary
-     *
-     * @param text The list of all words in the text.
-     * @param dictionary The list of all accepted words.
-     * @return String[] of all mispelled words in the order they appear in text. No duplicates.
-     */
     public String[] checkWords(String[] text, String[] dictionary) {
-
-        return null;
+        // Convert the dictionary to a set, create a set to store misspelled words.
+        Set<String> Dictionary_Set = new HashSet<>(Arrays.asList(dictionary));
+        Set<String> seen = new HashSet<>();
+        // Need an arraylist as well to keep the same order of misspelled words.
+        ArrayList<String> misspelled = new ArrayList<>();
+        // Iterate through the text, looking for any misspelled words that haven't been seen yet.
+        for (String word : text) {
+            if (!Dictionary_Set.contains(word) && !seen.contains(word)) {
+                seen.add(word);
+                misspelled.add(word);
+            }
+        }
+        // Convert the arraylist into an array, and return it.
+        String[] answer = new String[misspelled.size()];
+        for (int i = 0; i < misspelled.size(); i++) {
+            answer[i] = misspelled.get(i);
+        }
+        return answer;
     }
 }
