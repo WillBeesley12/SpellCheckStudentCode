03cd46793f6468546103bd70b1105f023b093471
williambeesley <wpbeesley@gmail.com>
2025-09-29 18:10:41 -0700

    v2 - A little efficiency fix

--- src/SpellCheck.java
+++ src/SpellCheck.java
@@ -10,23 +10,17 @@ import java.util.*;
 
 public class SpellCheck {
     public String[] checkWords(String[] text, String[] dictionary) {
-        // Convert the dictionary to a set, create a set to store misspelled words.
+        // Convert the dictionary to a set.
         Set<String> Dictionary_Set = new HashSet<>(Arrays.asList(dictionary));
-        Set<String> seen = new HashSet<>();
-        // Need an arraylist as well to keep the same order of misspelled words.
-        ArrayList<String> misspelled = new ArrayList<>();
+        // Create a Linked set to store misspelled words AND keep the order of the words.
+        LinkedHashSet<String> seen = new LinkedHashSet<>();
         // Iterate through the text, looking for any misspelled words that haven't been seen yet.
         for (String word : text) {
             if (!Dictionary_Set.contains(word) && !seen.contains(word)) {
                 seen.add(word);
-                misspelled.add(word);
             }
         }
-        // Convert the arraylist into an array, and then return it.
-        String[] answer = new String[misspelled.size()];
-        for (int i = 0; i < misspelled.size(); i++) {
-            answer[i] = misspelled.get(i);
-        }
-        return answer;
+        // Convert the seen set into an array
+        return (seen.toArray(new String[0]));
     }
 }
