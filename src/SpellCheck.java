9886269ac6c1f1cb670ecf478c6944b172f136d7
williambeesley <wpbeesley@gmail.com>
2025-10-02 13:56:48 -0700

    v4 - TST implementation started, not finished.

--- src/SpellCheck.java
+++ src/SpellCheck.java
@@ -75,12 +75,90 @@ class Trie {
         return current.isWord();
     }
 }
+// Class of Nodes specifically for TSTs. Main difference is storing of the previous node value.
+class TSTNode {
+    // These are the things I want to store: the letter, is it a word, and then the left middle and right nodes
+    char letter;
+    boolean isWord;
+    TSTNode left;
+    TSTNode middle;
+    TSTNode right;
+    // Initializes the first node
+    public TSTNode(char c) {
+        this.letter = c;
+        this.isWord = false;
+        this.left = null;
+        this.middle = null;
+        this.right = null;
+    }
+    // These two are self-explanatory
+    public boolean isWord() {
+        return isWord;
+    }
+    public void setWord() {
+        this.isWord = true;
+    }
+}
 
+// Class for TST
+class TST {
+    TSTNode root;
+    // To start a new TST, create the root node of the TST. I am using M to make the TST more balanced eventually
+    public TST() {
+        this.root = new TSTNode('m');
+    }
+    // This function inserts a new word into the TST
+    public void insert(String s) {
+        // Start at the top with the root node
+        TSTNode current = root;
+        // Iterate through each letter of the word except the last, as we don't need to check it (just add it)
+        for (int i = 0; i < s.length() - 1; i++) {
+            // Convert the character to an index
+            char c = s.charAt(i);
+            // If the letter is equal to the node, go straight down and place the next letter there
+            // Also set the new current node to the one we just made
+            if (current.letter == c) {
+                current.middle = new TSTNode(s.charAt(i + 1));
+                current = current.middle;
+            }
+            // This case makes us go right
+            else if (current.letter < c) {
+                current.right = new TSTNode(s.charAt(i + 1));
+                current = current.right;
+            }
+            // This case makes us go left
+            else {
+                current.left = new TSTNode(s.charAt(i + 1));
+                current = current.left;
+
+            }
+        }
+        // For the last letter in the word, set the isWord boolean to True.
+        current.setWord();
+    }
+    public boolean lookup(String s) {
+        // Again, start with the root node
+        TSTNode current = root;
+        // Index through each character of the word, checking if the next character exists
+        for (int i = 0; i < s.length(); i++) {
+            char c = s.charAt(i);
+            // If the next node doesn't exist, we can stop the function and just return false
+            if (current.next[index] == null) {
+                return false;
+            }
+            // Now grab the next node for the next iteration of the characters in the word
+            current = current.next[index];
+        }
+        // If it gets through every character, return the boolean stored under the last character in the word.
+        // This makes sure that it is a word, not just the start of a word.
+        return current.isWord();
+    }
+}
 
     public class SpellCheck {
         public String[] checkWords(String[] text, String[] dictionary) {
             // First, initialize our Trie and the Seen arraylist.
-            Trie data = new Trie();
+            TST data = new TST();
             ArrayList seen = new ArrayList();
             // Add all the words in the dictionary to the Trie
             for (int i = 0; i < dictionary.length; i++) {
